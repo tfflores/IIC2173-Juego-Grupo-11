69fd30581506095a0e1c2ba0c7d72be1c0623dcd
Lucas <lamarin@uc.cl>
2019-10-17 17:21:06 -0700

    add menus

--- 2D Shooter/Assets/PlayerMovement.cs
+++ 2D Shooter/Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 6.5f;
 
     public Rigidbody2D rb;
     public Camera cam;
